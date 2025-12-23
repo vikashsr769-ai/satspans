ba624fb8bc4e7105175d346bc74a699933b2a6f0
vikashsr769-ai <vikashsr769@gmail.com>
2025-12-23 17:54:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://xeafepvajcaloumjmpoe.supabase.co', 'sb_publishable_v4-A8g2PFaM__MWxbYjDfg_cA2m3MYB');
+Console.log(supabase);
+const Login = document.getElementById('Login');
 
+const name = document.getElementById('name');
+const dept = document.getElementById('dept');
+const email = document.getElementById('email');
+const pass = document.getElementById('pass');
+
+Login.onclick = () =>{
+      
+
+}
